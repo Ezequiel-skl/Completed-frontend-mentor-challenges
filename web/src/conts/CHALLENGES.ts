42f028d2a9b5a6528572421545ccc48d428774af
ezequiel-sk <ezequielsmiak@gmail.com>
2024-07-04 15:50:55 -0300

    first commit

--- web/src/conts/CHALLENGES.ts
+++ web/src/conts/CHALLENGES.ts
@@ -1,85 +1,165 @@
 import type { Challenge } from "@/types/challenge"
-export const CHALLENGES: Challenge[] = [
-	{
-		slug: "coding-bootcamp-testimonials-slider",
-		title: "Coding bootcamp testimonials slider",
-		description:
-			"In this challenge, I developed a testimonial slider using HTML, CSS, and JavaScript. The main goal was to create an attractive and functional interface that would allow users to browse through different customer reviews.",
-		difficulty: ["Junior", 2],
-		languages: ["HTML", "CSS", "JS"],
-		tags: ["HTML&CSS", "JS", "Newbie"],
-		isNew: true,
-	},
-	{
-		slug: "faq-accordion",
-		title: "FAQ Accordion",
-		description:
-			"In this challenge, you'll build an FAQ accordion. This is an extremely common front-end pattern, so it's an excellent opportunity to get some practice in!",
-		difficulty: ["Newbie", 1],
-		languages: ["HTML", "CSS", "JS"],
-		tags: ["HTML&CSS", "JS", "Newbie"],
-	},
-	{
-		slug: "fylo-data-storage-component",
-		title: "Fylo data storage component",
-		description:
-			"I developed this data storage component matching the original design. It was a great opportunity to hone my skills in manipulating data and implementing precise styles.",
-		difficulty: ["Junior", 2],
-		languages: ["HTML", "CSS"],
-		tags: ["HTML&CSS", "Junior"],
-	},
-	{
-		slug: "interactive-rating-component",
-		title: "Interactive rating component",
-		description:
-			"Developed an interactive ranking page with HTML, CSS and JavaScript. Practiced user interaction management and dynamic DOM updating.",
-		difficulty: ["Newbie", 1],
-		languages: ["HTML", "CSS", "JS"],
-		tags: ["HTML&CSS", "JS", "Newbie"],
-	},
-	{
-		slug: "qr-code-component",
-		title: "QR Code Component",
-		description:
-			"I developed my layout and styling skills by creating a QR code card. I learned how to structure HTML efficiently and apply CSS for a clean and responsive design.",
-		difficulty: ["Newbie", 1],
-		languages: ["HTML", "CSS"],
-		tags: ["HTML&CSS", "Newbie"],
-	},
-	{
-		slug: "recipe-page",
-		title: "Recipe page",
-		description:
-			"I developed my design and styling skills by creating a simple recipe page. I improved my skills in responsive design and semantic HTML.",
-		difficulty: ["Newbie", 1],
-		languages: ["HTML", "CSS"],
-		tags: ["HTML&CSS", "Newbie"],
-	},
-	{
-		slug: "social-links-profile",
-		title: "Social links profile",
-		description:
-			"I developed my layout and styling skills by creating a social link profile. I learned how to structure HTML efficiently and apply CSS for a clean and responsive design.",
-		difficulty: ["Newbie", 1],
-		languages: ["HTML", "CSS"],
-		tags: ["HTML&CSS", "Newbie"],
-	},
-	{
-		slug: "social-media-dashboard-with-theme-switcher",
-		title: "Social media dashboard with theme switcher",
-		description:
-			"I created a panel that adjusts the layout based on the device's screen size. Users can see hover states and change the color theme according to their preference.",
-		difficulty: ["Junior", 2],
-		languages: ["HTML", "CSS", "JS"],
-		tags: ["HTML&CSS", "JS", "Junior"],
-	},
-	{
-		slug: "testimonials-grid-section",
-		title: "Testimonials grid section",
-		description:
-			"I developed a testimonials section using HTML and CSS. I learned how to create responsive layouts and handle media queries with CSS.",
-		difficulty: ["Junior", 2],
-		languages: ["HTML", "CSS"],
-		tags: ["HTML&CSS", "Junior"],
-	},
-]
+ export const CHALLENGES: Challenge[] = [
+  {
+    "slug": "coding-bootcamp-testimonials-slider",
+    "title": "Coding bootcamp testimonials slider",
+    "description": "In this challenge, I developed a testimonial slider using HTML, CSS, and JavaScript. The main goal was to create an attractive and functional interface that would allow users to browse through different customer reviews.",
+    "difficulty": [
+      "Junior",
+      2
+    ],
+    "languages": [
+      "HTML",
+      "CSS",
+      "JS"
+    ],
+    "tags": [
+      "HTML&CSS",
+      "JS",
+      "Newbie"
+    ]
+  },
+  {
+    "slug": "faq-accordion",
+    "title": "FAQ Accordion",
+    "description": "In this challenge, you'll build an FAQ accordion. This is an extremely common front-end pattern, so it's an excellent opportunity to get some practice in!",
+    "difficulty": [
+      "Newbie",
+      1
+    ],
+    "languages": [
+      "HTML",
+      "CSS",
+      "JS"
+    ],
+    "tags": [
+      "HTML&CSS",
+      "JS",
+      "Newbie"
+    ]
+  },
+  {
+    "slug": "fylo-data-storage-component",
+    "title": "Fylo data storage component",
+    "description": "I developed this data storage component matching the original design. It was a great opportunity to hone my skills in manipulating data and implementing precise styles.",
+    "difficulty": [
+      "Junior",
+      2
+    ],
+    "languages": [
+      "HTML",
+      "CSS"
+    ],
+    "tags": [
+      "HTML&CSS",
+      "Junior"
+    ]
+  },
+  {
+    "slug": "interactive-rating-component",
+    "title": "Interactive rating component",
+    "description": "Developed an interactive ranking page with HTML, CSS and JavaScript. Practiced user interaction management and dynamic DOM updating.",
+    "difficulty": [
+      "Newbie",
+      1
+    ],
+    "languages": [
+      "HTML",
+      "CSS",
+      "JS"
+    ],
+    "tags": [
+      "HTML&CSS",
+      "JS",
+      "Newbie"
+    ]
+  },
+  {
+    "slug": "qr-code-component",
+    "title": "QR Code Component",
+    "description": "I developed my layout and styling skills by creating a QR code card. I learned how to structure HTML efficiently and apply CSS for a clean and responsive design.",
+    "difficulty": [
+      "Newbie",
+      1
+    ],
+    "languages": [
+      "HTML",
+      "CSS"
+    ],
+    "tags": [
+      "HTML&CSS",
+      "Newbie"
+    ]
+  },
+  {
+    "slug": "recipe-page",
+    "title": "Recipe page",
+    "description": "I developed my design and styling skills by creating a simple recipe page. I improved my skills in responsive design and semantic HTML.",
+    "difficulty": [
+      "Newbie",
+      1
+    ],
+    "languages": [
+      "HTML",
+      "CSS"
+    ],
+    "tags": [
+      "HTML&CSS",
+      "Newbie"
+    ]
+  },
+  {
+    "slug": "social-links-profile",
+    "title": "Social links profile",
+    "description": "I developed my layout and styling skills by creating a social link profile. I learned how to structure HTML efficiently and apply CSS for a clean and responsive design.",
+    "difficulty": [
+      "Newbie",
+      1
+    ],
+    "languages": [
+      "HTML",
+      "CSS"
+    ],
+    "tags": [
+      "HTML&CSS",
+      "Newbie"
+    ]
+  },
+  {
+    "slug": "social-media-dashboard-with-theme-switcher",
+    "title": "Social media dashboard with theme switcher",
+    "description": "I created a panel that adjusts the layout based on the device's screen size. Users can see hover states and change the color theme according to their preference.",
+    "difficulty": [
+      "Junior",
+      2
+    ],
+    "languages": [
+      "HTML",
+      "CSS",
+      "JS"
+    ],
+    "tags": [
+      "HTML&CSS",
+      "JS",
+      "Junior"
+    ]
+  },
+  {
+    "slug": "testimonials-grid-section",
+    "title": "Testimonials grid section",
+    "description": "I developed a testimonials section using HTML and CSS. I learned how to create responsive layouts and handle media queries with CSS.",
+    "difficulty": [
+      "Junior",
+      2
+    ],
+    "languages": [
+      "HTML",
+      "CSS"
+    ],
+    "tags": [
+      "HTML&CSS",
+      "Junior"
+    ],
+    "isNew": true
+  }
+];
