8e55575b3710d2066d1a9237d95608eaa376d433
ezequiel-sk <ezequielsmiak@gmail.com>
2024-07-05 14:49:46 -0300

    Add vercel configurations

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,12 +1,64 @@
 import { defineConfig } from "astro/config"
-
 import tailwind from "@astrojs/tailwind"
 import react from "@astrojs/react"
+import { VitePWA } from "vite-plugin-pwa"
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
+	build: {
+		inlineStylesheets: "always",
+	},
+	compressHTML: true,
+	prefetch: true,
 	integrations: [tailwind(), react()],
 	devToolbar: {
 		enabled: false,
+	},
+	output: "server",
+	adapter: vercel(),
+	output: "server",
+	vite: {
+		build: {
+			cssMinify: "lightningcss",
+		},
+		ssr: {
+			noExternal: ["path-to-regexp"],
+		},
+		plugins: [
+			VitePWA({
+				registerType: "autoUpdate",
+				manifest,
+				workbox: {
+					globDirectory: ".vercel/output/static",
+					globPatterns: ["**/*.{html,js,css,woff,woff2,ttf,eot,ico}"],
+					runtimeCaching: [
+						{
+							urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
+							handler: "CacheFirst",
+							options: {
+								cacheName: "images",
+								expiration: {
+									maxEntries: 100,
+									maxAgeSeconds: 30 * 24 * 60 * 60,
+								},
+							},
+						},
+						{
+							urlPattern: /\.(?:woff|woff2|ttf|eot|ico)$/,
+							handler: "CacheFirst",
+							options: {
+								cacheName: "fonts",
+								expiration: {
+									maxEntries: 10,
+									maxAgeSeconds: 30 * 24 * 60 * 60,
+								},
+							},
+						},
+					],
+					navigateFallback: null,
+				},
+			}),
+		],
 	},
 })
